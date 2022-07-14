309a056433dac819e2daac17a2d0694ed8659c8a
alex <alexzelinsky124@gmail.com>
2022-07-14 12:47:31 -0700

    phase 1

--- src/components/Greenhouse/Greenhouse.js
+++ src/components/Greenhouse/Greenhouse.js
@@ -1,18 +1,24 @@
-import dayImage from './images/greenhouse-day.jpg';
-import nightImage from './images/greenhouse-night.jpg';
-import './Greenhouse.css';
+import dayImage from "./images/greenhouse-day.jpg";
+import nightImage from "./images/greenhouse-night.jpg";
+import "./Greenhouse.css";
 
-import LightSwitch from './LightSwitch';
-import ClimateStats from './ClimateStats';
+import LightSwitch from "./LightSwitch";
+import ClimateStats from "./ClimateStats";
+
+import { useTheme } from "../../context/ThemeContext";
 
 function Greenhouse() {
+  const { themeName } = useTheme();
+  console.log(themeName);
 
   return (
     <section>
-      <img  className='greenhouse-img'
-            src={dayImage}
-            alt='greenhouse' 
-      />
+      {themeName === "day" ? (
+        <img className="greenhouse-img" src={dayImage} alt="greenhouse" />
+      ) : (
+        <img className="greenhouse-img" src={nightImage} alt="greenhouse" />
+      )}
+
       <LightSwitch />
       <ClimateStats />
     </section>
